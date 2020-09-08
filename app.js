53e8dd106b6562ac51ad705fad58c3f1ba467766
shekhar <shekharpratapsingh26@gmail.com>
2020-09-08 18:40:35 +0530

    change index to app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express()
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.send('hello world')
+  res.send('<h1> hello world </h1>')
 })
 app.listen(3000, ()=>{
   console.log(`app is running in port 3000`)
